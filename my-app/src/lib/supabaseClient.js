03fa87d2ed2e60bc469dff3d6731ae6c25434497
Hector Grandon <hgrandon@live.cl>
2025-10-22 20:44:44 -0300

    fix: usar URL y anon key directas en supabaseClient

--- my-app/src/lib/supabaseClient.js
+++ my-app/src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://vmwhngvffpmrkkuvtzoe.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlIjoiN3JtYmpvbnYxMW9sWTF0aHNjczMzZ0lGZTlZIn0.gWBWvE3rVGLvMfxV_NH2gUG4n7aN2ZfC2T2c7vN8m5Y'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
